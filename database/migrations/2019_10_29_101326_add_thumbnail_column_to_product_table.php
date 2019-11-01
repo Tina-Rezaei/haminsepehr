c1a6ed368f89427fc4f56bdef730372ea89c96ac
tina rezaei <tina.re1374@yahoo.com>
2019-11-01 23:04:47 +0330

    comment related products

---
+++ database/migrations/2019_10_29_101326_add_thumbnail_column_to_product_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddThumbnailColumnToProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('thumbnail_img',200);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product', function (Blueprint $table) {
+            //
+        });
+    }
+}
